a66287058f47ed8f8a6011751c9d5e97a35b71c0
mraiyuu <munenedenis618@gmail.com>
2024-11-17 10:42:40 +0300

    redis database connection

--- application/app.go
+++ application/app.go
@@ -1,18 +1,63 @@
 package application
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
-
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
 }
 
+func (a *App) Start(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: a.router,
+	}
 
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		fmt.Errorf("failed to connect to redis: %w", err)
+	}
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("starting server ")
+
+	ch := make(chan error, 1)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		ch <- fmt.Errorf("failed to start server %w", err)
+	}
+	close(ch)
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancle := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancle()
+		return server.Shutdown(timeout)
+	}
+
+	return nil
+}
